066c598348500dbc5532c805f5c256044ed06c8a
Vikrant Chaudhary <vikrant.chaudhary@BLR-Vikrant.local>
2022-03-13 11:21:55 +0530

    added ExpenseDate components and props

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,12 +1,13 @@
 import "./ExpenseItem.css";
+import ExpenseDate from "./ExpenseDate.js";
 
-function ExpenseItem() {
+function ExpenseItem(props) {
   return (
     <div className="expense-item">
-      <div>March 28th 2021</div>
-      <div className="expense-item_description">
-        <h2>Car Insurance</h2>
-        <div className="expense-item_price">$294.67</div>
+      <ExpenseDate date={props.date} />
+      <div className="expense-item__description">
+        <h2>{props.title}</h2>
+        <div className="expense-item__price">${props.amount}</div>
       </div>
     </div>
   );
